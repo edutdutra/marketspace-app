e066b8ce092ab3bb2cac958927b94665e43ee848
Eduardo-Dutra <edutdutra@gmail.com>
2023-02-28 08:59:34 -0300

    add logo in sign in screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,17 @@
-import {ScrollView, Text, VStack} from "native-base";
+import {Center, ScrollView, Text, VStack} from "native-base";
+
+import LogoSvg from "@assets/logo.svg"
+import TitleSvg from "@assets/title.svg"
 
 export function SignIn() {
     return (
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} px={10} pb={16}>
-                <Text>SIGN IN</Text>
+                <Center my={24}>
+                    <LogoSvg/>
+                    <TitleSvg/>
+                    <Text color="gray.300" fontSize="md">Seu espaço de compra e venda</Text>
+                </Center>
             </VStack>
         </ScrollView>
     )
