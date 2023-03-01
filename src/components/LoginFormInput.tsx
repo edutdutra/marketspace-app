acb7641564223146d7b558593398a5a2b3611b12
Eduardo-Dutra <edutdutra@gmail.com>
2023-02-28 21:21:16 -0300

    add login form input component

--- src/components/LoginFormInput.tsx
+++ src/components/LoginFormInput.tsx
@@ -1,3 +1,40 @@
-export function LoginFormInput() {
+import {Box, FormControl, IInputProps, Input, useTheme} from "native-base";
+import {Eye} from "phosphor-react-native";
+import {TouchableOpacity} from "react-native";
 
+type Props = IInputProps & {
+    isPassword?: boolean;
+}
+
+export function LoginFormInput({isPassword = false, ...rest}: Props) {
+    const {colors} = useTheme();
+
+    return (
+        <FormControl mb={4}>
+            <Input
+                backgroundColor="gray.700"
+                h={12}
+                px={4}
+                borderRadius={6}
+                borderWidth={0}
+                fontFamily="body"
+                fontSize="lg"
+                color="gray.200"
+                placeholderTextColor="gray.400"
+                _focus={{
+                    borderWidth: 1,
+                    borderColor: "gray.300"
+                }}
+                InputRightElement={isPassword ?
+                    <Box mr={4}>
+                        <TouchableOpacity>
+                            <Eye size={20} color={colors.gray[300]}/>
+                        </TouchableOpacity>
+                    </Box>
+                    : <></>
+                }
+                {...rest}
+            />
+        </FormControl>
+    )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,16 +2,38 @@ import {Center, ScrollView, Text, VStack} from "native-base";
 
 import LogoSvg from "@assets/logo.svg"
 import TitleSvg from "@assets/title.svg"
+import {LoginFormInput} from "@components/LoginFormInput";
 
 export function SignIn() {
     return (
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} px={10} pb={16}>
+
                 <Center my={24}>
                     <LogoSvg/>
                     <TitleSvg/>
                     <Text color="gray.300" fontSize="md">Seu espaço de compra e venda</Text>
                 </Center>
+
+                <Center>
+                    <Text
+                        color="gray.200"
+                        fontFamily="body"
+                        fontSize="md"
+                        mb={4}
+                    >
+                        Acesse sua conta
+                    </Text>
+
+                    <LoginFormInput placeholder="E-mail"/>
+                    <LoginFormInput isPassword placeholder="Senha"/>
+
+                </Center>
+
+                <Center>
+
+                </Center>
+
             </VStack>
         </ScrollView>
     )
